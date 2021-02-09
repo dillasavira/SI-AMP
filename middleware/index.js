ad45b2fca2eb72eaee331b61684c6f6c4fed1467
Dilla Savira <dillasavira00@gmail.com>
2021-02-09 14:01:40 +0700

    menambahkan role berhasil

--- middleware/index.js
+++ middleware/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var auth = require('./auth');
+const verifikasi = require('./verifikasi');
 var router = express.Router();
 
 //daftarkan menu registrasi
 router.post('/api/v1/register', auth.registrasi);
 router.post('/api/v1/login', auth.login);
 
+//alamat yang perlu ontorisasi
+router.get('/api/v1/rahasia', verifikasi(), auth.halamanrahasia);
 
 
 module.exports = router;
